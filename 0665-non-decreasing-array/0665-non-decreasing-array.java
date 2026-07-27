b5222aa04ce6f2a7486e436e75ba24861b13272f
Praveen_Sivakumar <praveensv1209@gmail.com>
2026-07-27 11:16:19 +0530

    Time: 0 ms (100%), Space: 47.2 MB (25.36%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPossibility(int[] a) {
+        int modified = 0;
+        for (int i = 1, prev = a[0]; i < a.length; i++) {
+            if (a[i] < prev) {
+                if (modified++ > 0) return false;
+                if (i - 2 >= 0 && a[i - 2] > a[i]) continue;
+            }
+            prev = a[i];
+        }
+        return true;
+    }
+}
