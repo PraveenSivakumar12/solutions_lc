8d9bf89bde9d8d981e0c0d4abda97a9077ed91e9
Praveen_Sivakumar <praveensv1209@gmail.com>
2026-07-28 14:10:35 +0530

    Time: 323 ms (30.22%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-SELECT w.name,w.population,w.area
-FROM WORLD w
-WHERE area>=3000000 OR population>=25000000; 
+SELECT w.name,w.population,w.area FROM WORLD w WHERE area>=3000000 OR population>=25000000; 
